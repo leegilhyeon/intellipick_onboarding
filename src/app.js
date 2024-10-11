2cb26d2b571fc97b72c0155100a3575fb73d4c96
gil-hyeon <lgh5498@naver.com>
2024-10-11 19:50:46 +0900

    스웨거 설정 추가

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import UserRouter from "./routers/user.router.js";
+import { swaggerUi, spec } from "./swagger.js";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(
+  "/api",
+  [UserRouter],
+  swaggerUi.serve,
+  swaggerUi.setup(spec, { explorer: true })
+);
+
+const PORT = process.env.SERVER_PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello node!");
+});
+
+app.listen(PORT, () => {
+  console.log(`${PORT} 서버가 열렸어요`);
+});
---
+++ src/swagger.js
@@ -0,0 +1,19 @@
+import swaggerUi from "swagger-ui-express";
+import swaggerJSDoc from "swagger-jsdoc";
+
+const swagger = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "회원가입 및 로그인",
+      version: "1.0.0",
+      description: "회원가입, 로그인, 토큰 발급 API",
+    },
+    servers: [{ url: "http://localhost:3000/api" }],
+  },
+  apis: ["./routers/*.js"],
+};
+
+const spec = swaggerJSDoc(swagger);
+
+export { swaggerUi, spec };
