4c110655a846264b17a972efcfa11aeab5a70411
gil-hyeon <lgh5498@naver.com>
2024-10-11 22:36:37 +0900

    app.js 파일 이동

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-import UserRouter from "./routers/user.router.js";
-import { swaggerUi, spec } from "./swagger.js";
+import UserRouter from "./src/routers/user.router.js";
+import { swaggerUi, spec } from "./src/swagger.js";
 
 const app = express();
 app.use(express.json());
