509ac630c5a6e6513eaeae88c4f0870f416d2db4
gil-hyeon <lgh5498@naver.com>
2024-11-08 16:30:07 +0900

    토큰 수정 부분

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -3,6 +3,7 @@ import jwt from "jsonwebtoken";
 import { PrismaClient } from "@prisma/client";
 import "dotenv/config";
 import bcrypt from "bcrypt";
+import { verifyRefreshToken } from "../jwt.middleware.js/refresh-token.middleware.js";
 
 const router = express.Router();
 const prisma = new PrismaClient();
@@ -41,23 +42,27 @@ const prisma = new PrismaClient();
 
 //회원가입
 router.post("/sign-up", async (req, res, next) => {
-  const { email, nickname, password, passwordConfirm } = req.body;
+  try {
+    const { email, nickname, password, passwordConfirm } = req.body;
 
-  const userCheck = await prisma.user.findUnique({ where: { email } });
-  if (userCheck) {
-    return res.status(409).json({ message: "이미 존재하는 이메일입니다." });
+    const userCheck = await prisma.user.findUnique({ where: { email } });
+    if (userCheck) {
+      return res.status(409).json({ message: "이미 존재하는 이메일입니다." });
+    }
+    if (password !== passwordConfirm) {
+      return res.status(400).json({ message: "비밀번호를 확인해주세요." });
+    }
+    // const hashedRound = process.env.HASH_SALT_ROUNDS;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await prisma.user.create({
+      data: { email, password: hashedPassword, nickname },
+    });
+    return res
+      .status(201)
+      .json({ email, nickname, message: "회원가입이 완료되었습니다." });
+  } catch (error) {
+    next(error);
   }
-  if (password !== passwordConfirm) {
-    return res.status(400).json({ message: "비밀번호를 확인해주세요." });
-  }
-  // const hashedRound = process.env.HASH_SALT_ROUNDS;
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const user = await prisma.user.create({
-    data: { email, password: hashedPassword, nickname },
-  });
-  return res
-    .status(201)
-    .json({ email, nickname, message: "회원가입이 완료되었습니다." });
 });
 /**
  * @swagger
@@ -106,9 +111,61 @@ router.post("/sign-in", async (req, res) => {
   if (!passwordCheck) {
     return res.status(401).json({ message: "비밀번호가 일치하지 않습니다." });
   }
-  const userId = userCheck.id;
-  const payload = { id: userId };
+  //const userId = userCheck.id;
+  const payload = userCheck.id;
+  console.log(payload, "여기");
+  const token = await authTokens({ id: payload });
   //토큰
+
+  return res.status(200).json({ token });
+});
+
+/**
+ * @swagger
+ * /api/token:
+ *   post:
+ *     summary: 토큰 재발급
+ *     description: 토큰 재발급 진행
+ *     parameters:
+ *       - in: header
+ *         name: Authorization
+ *         required: true
+ *         description: Bearer {token} 형식의 인증 토큰
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: 토큰 재발급 성공
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "토큰이 재발급 되었습니다."
+ *                 newToken:
+ *                   type: string
+ *                   description: 새로 발급된 토큰
+ *       400:
+ *         description: 토큰 재발급 실패
+ */
+
+//재발급
+router.post("/token", verifyRefreshToken, async (req, res) => {
+  const user = req.user;
+
+  const payload = { id: user.id };
+  console.log(payload, "확인");
+  const newToken = await authTokens(payload);
+  return res
+    .status(200)
+    .json({ message: "토큰이 재발급 되었습니다.", newToken });
+});
+
+//토큰 생성 함수
+const authTokens = async (payload) => {
+  const userId = payload.id;
   const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
     expiresIn: "12h",
   });
@@ -126,13 +183,7 @@ router.post("/sign-in", async (req, res) => {
       refreshToken: hashedRefreshToken,
     },
   });
-  return res.status(200).json({ accessToken, refreshToken });
-});
-
-//토큰 생성
-// authTokens = async (payload) => {
-//   const userId = payload.id;
-
-// };
+  return { accessToken, refreshToken };
+};
 
 export default router;
